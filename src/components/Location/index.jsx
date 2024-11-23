1bcede063a2642a81d842b360951b84679020844
Amit Sankhyan <amitcrelookup@gmail.com>
2024-11-23 17:14:08 +0530

    Your commit message

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -20,24 +20,46 @@ const index = ({ isModalOpen, closeModal }) => {
           padding: "20px",
           borderRadius: "10px",
           textAlign: "center",
+          height: "250px", // Ensure height is in a string for inline CSS
+          display: "flex", // Use string values
+          flexDirection: "column", // Use camelCase
+          justifyContent: "center", // Use camelCase
+          alignItems: "center", // Use camelCase
         },
       }}
     >
-      <h2>Modal Title</h2>
-      <p>This is the content inside the modal.</p>
+      <h2 style={{fontSize: "20px", }} >Select a location for delivery</h2>
+      <p>Choose your address location to see product availability and delivery options</p>
+      <div className="serchArea_Head" style={{ position:"relative",    width: "100%",}}>
+      <svg height={18} width={18} class="svg-icon search-icon"  style={{position: "absolute", left: "10px", top:"34%", }} aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">A magnifying glass icon.</desc><g class="search-path" fill="none" stroke="#848F91"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
+  <input type="text" placeholder="Search for area or street name" style={{ paddingLeft: "32px", paddingRight: "32px", }}  />
+</div>
       <button
         onClick={closeModal}
         style={{
-          padding: "10px 20px",
-          backgroundColor: "blue",
+          padding: "4px",
           color: "white",
           border: "none",
           borderRadius: "5px",
           cursor: "pointer",
           marginTop: "15px",
+          position: "absolute",
+          top: "5px",
+          right: "6px",
+          marginTop: "4px",
         }}
       >
-        Close
+        <svg width={14} height={14} viewPort="0 0 12 12" version="1.1"
+     xmlns="http://www.w3.org/2000/svg">
+    <line x1="1" y1="11" 
+          x2="11" y2="1" 
+          stroke="black" 
+          stroke-width="2"/>
+    <line x1="1" y1="1" 
+          x2="11" y2="11" 
+          stroke="black" 
+          stroke-width="2"/>
+</svg>
       </button>
     </Modal>
   );
--- src/components/categories/beauty-category.jsx
+++ src/components/categories/beauty-category.jsx
@@ -76,19 +76,42 @@ const BeautyCategory = () => {
       <section className="tp-category-area pt-95">
         <div className="container">
           <div className="row align-items-end">
-            <div className="col-lg-6 col-md-8">
-              <div className="tp-section-title-wrapper-3 mb-45">
+            <div className="col-lg-6 col-md-12">
+              <div className="tp-section-title-wrapper-3 mb-30">
                 <span className="tp-section-title-pre-3">
                   Product Collection
                 </span>
-                <h3 className="tp-section-title-3">Discover our products</h3>
+                <h3 className="tp-section-title-3">Discover our products ss</h3>
               </div>
             </div>
-            <div className="col-lg-6 col-md-4">
-              <div className="tp-category-more-3 text-md-end mb-55">
-                <Link href="/shop" className="tp-btn">
-                  Shop All Products <ArrowRightSmTwo />
-                </Link>
+            <div className="col-lg-6 col-md-12">
+              <div className="tp-category-more-3 text-md-end mb-35">
+                <div className="col-md-8 ms-auto">
+                <div className="row  align-items-center">
+                <div className="col-8 col-md-8">
+                  <div className="d-flex justify-content-end justify-content-lg-center">
+                  <Link href="/shop" className="tp-btn">
+                    <span className="me-1">  Shop All Products</span>
+                     <ArrowRightSmTwo />
+                  </Link>
+                  </div>
+                </div>
+                <div className="col-4 col-md-4">
+                   <div className="viweSlideIcons d-flex justify-content-end">
+                       <div className="viweLeftIcon me-2">
+                        <button className="btn shadow-sm" type="button">
+                          <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.829 19a.998.998 0 0 1-.78-.373l-4.828-6a1 1 0 0 1 .01-1.267l5-6a1 1 0 1 1 1.536 1.28l-4.474 5.371 4.315 5.362a1 1 0 0 1-.78 1.627Z" fill="#231F20"></path><mask id="arrow-ios-left_svg__a" mask-type="alpha" maskUnits="userSpaceOnUse" x="8" y="5" width="7" height="14"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.829 19a.998.998 0 0 1-.78-.373l-4.828-6a1 1 0 0 1 .01-1.267l5-6a1 1 0 1 1 1.536 1.28l-4.474 5.371 4.315 5.362a1 1 0 0 1-.78 1.627Z" fill="#fff"></path></mask><g mask="url(#arrow-ios-left_svg__a)"><path fill="#606060" d="M0 0h24v24H0z"></path></g></svg>
+                       </button>
+                        </div>
+                        <div className="viweRightIcon">
+                        <button className="btn shadow-sm" type="button">
+                        <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10 19a1 1 0 0 1-.768-1.64l4.476-5.371-4.316-5.362a1 1 0 0 1 1.56-1.254l4.828 6a1 1 0 0 1-.011 1.267l-5 6a1 1 0 0 1-.77.36Z" fill="#231F20"></path><mask id="arrow-ios-right_svg__a" mask-type="alpha" maskUnits="userSpaceOnUse" x="9" y="5" width="8" height="14"><path fill-rule="evenodd" clip-rule="evenodd" d="M10 19a1 1 0 0 1-.768-1.64l4.476-5.371-4.316-5.362a1 1 0 0 1 1.56-1.254l4.828 6a1 1 0 0 1-.011 1.267l-5 6a1 1 0 0 1-.77.36Z" fill="#fff"></path></mask><g mask="url(#arrow-ios-right_svg__a)"><path fill="#606060" d="M0 0h24v24H0z"></path></g></svg>
+                        </button>
+                       </div>
+                   </div>
+                </div>
+                </div>
+                </div>
               </div>
             </div>
           </div>
